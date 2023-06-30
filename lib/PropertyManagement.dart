091db49f7eaa0252afa9da66f466cac6f5b6cdf1
Niggling <272744476@qq.com>
2023-06-30 11:01:29 +0800

    事件处理和房屋管理更新

--- lib/PropertyManagement.dart
+++ lib/PropertyManagement.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '/database_helper.dart';
 
 class PropertyManagementPage extends StatefulWidget {
   @override
@@ -7,22 +6,49 @@ class PropertyManagementPage extends StatefulWidget {
 }
 
 class _PropertyManagementPageState extends State<PropertyManagementPage> {
-  List<Map<String, dynamic>> applicationList = []; // 物业申请列表
-
-  @override
-  void initState() {
-    super.initState();
-    _loadApplications();
-  }
-
-  void _loadApplications() async {
-    final dbHelper = DatabaseHelper.instance;
-    final list = await dbHelper.getApplicationList();
-    setState(() {
-      applicationList = list;
-    });
-  }
-
+  List<Map<String, dynamic>> applicationList = [
+    {
+      'id': 1,
+      'owner': '张三',
+      'community': '小区A',
+      'building': '1栋',
+      'unit': '1单元',
+      'house': '101',
+    },
+    {
+      'id': 2,
+      'owner': '李四',
+      'community': '小区B',
+      'building': '2栋',
+      'unit': '2单元',
+      'house': '202',
+    },
+    {
+      'id': 3,
+      'owner': '王五',
+      'community': '小区C',
+      'building': '3栋',
+      'unit': '3单元',
+      'house': '303',
+    },
+    // 更多的假数据...
+    {
+      'id': 4,
+      'owner': '赵六',
+      'community': '小区D',
+      'building': '4栋',
+      'unit': '4单元',
+      'house': '404',
+    },
+    {
+      'id': 5,
+      'owner': '钱七',
+      'community': '小区E',
+      'building': '5栋',
+      'unit': '5单元',
+      'house': '505',
+    },
+  ]; // 物业申请列表
 
   void _handleApplication(int applicationId, bool approved) {
     // 处理申请，根据approved确定是否同意
@@ -31,6 +57,10 @@ class _PropertyManagementPageState extends State<PropertyManagementPage> {
     } else {
       print('不同意申请');
     }
+
+    setState(() {
+      applicationList.removeWhere((application) => application['id'] == applicationId);
+    });
 
     // TODO: 更新数据库中的申请状态
   }
