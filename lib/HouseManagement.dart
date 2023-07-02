400e79ab28d5c5b0ce756840b4d72f05e8522ce5
Niggling <272744476@qq.com>
2023-07-02 18:27:03 +0800

    房屋绑定界面更新

--- lib/HouseManagement.dart
+++ lib/HouseManagement.dart
@@ -42,119 +42,175 @@ class _HouseManagementPageState extends State<HouseManagementPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('房屋管理'),
+        backgroundColor: Colors.blue,
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '选择小区:',
-              style: TextStyle(fontSize: 18),
+          Card(
+          elevation: 5,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  '选择小区:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                DropdownButton<String>(
+                  value: selectedCommunity,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedCommunity = newValue;
+                    });
+                  },
+                  items: <String>[
+                    '翡翠湖小区',
+                    '屯溪路小区',
+                    '合工大小区',
+                    '中科大小区',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ],
             ),
-            DropdownButton<String>(
-              value: selectedCommunity,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedCommunity = newValue;
-                });
-              },
-              items: <String>[
-                '翡翠湖小区',
-                '屯溪路小区',
-                '合工大小区',
-                '中科大小区',
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
+          ),
+        ),
+        SizedBox(height: 16),
+        Card(
+          elevation: 5,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  '选择楼栋:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                DropdownButton<String>(
+                  value: selectedBuilding,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedBuilding = newValue;
+                    });
+                  },
+                  items: <String>[
+                    '1号楼',
+                    '2号楼',
+                    '3号楼',
+                    '4号楼',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ],
             ),
-            SizedBox(height: 16),
-            Text(
-              '选择楼栋:',
-              style: TextStyle(fontSize: 18),
+          ),
+        ),
+        SizedBox(height: 16),
+        Card(
+          elevation: 5,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  '选择单元:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                DropdownButton<String>(
+                  value: selectedUnit,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedUnit = newValue;
+                    });
+                  },
+                  items: <String>[
+                    '1单元',
+                    '2单元',
+                    '3单元',
+                    '4单元',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ],
             ),
-            DropdownButton<String>(
-              value: selectedBuilding,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedBuilding = newValue;
-                });
-              },
-              items: <String>[
-                '1号楼',
-                '2号楼',
-                '3号楼',
-                '4号楼',
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 16),
-            Text(
-              '选择单元:',
-              style: TextStyle(fontSize: 18),
-            ),
-            DropdownButton<String>(
-              value: selectedUnit,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedUnit = newValue;
-                });
-              },
-              items: <String>[
-                '1单元',
-                '2单元',
-                '3单元',
-                '4单元',
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 16),
-            Text(
+          ),
+        ),
+        SizedBox(height: 16),
+        Card(
+          elevation: 5,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+              Text(
               '选择户号:',
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+            SizedBox(height: 8),
             DropdownButton<String>(
-              value: selectedHouse,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedHouse = newValue;
-                });
-              },
-              items: <String>[
-                '101',
-                '102',
-                '201',
-                '202',
-                '301',
-                '302',
-                '401',
-                '402',
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _bindHouse,
-              child: Text('绑定房屋'),
-            ),
+                value: selectedHouse                      ,onChanged: (String? newValue) {
+          setState(() {
+          selectedHouse = newValue;
+          });
+          },
+            items: <String>[
+              '101',
+              '102',
+              '201',
+              '202',
+              '301',
+              '302',
+              '401',
+              '402',
+            ].map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
           ],
         ),
       ),
+    ),
+    SizedBox(height: 16),
+    ElevatedButton(
+    onPressed: _bindHouse,
+    child: Text(
+    '绑定房屋',
+    style: TextStyle(fontSize: 18),
+    ),
+    style: ElevatedButton.styleFrom(
+    primary: Colors.white,
+    onPrimary: Colors.black,
+    elevation: 5,
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(5),
+    ),
+    ),
+    ),
+    ],
+    ),
+    ),
     );
   }
 }
@@ -162,5 +218,9 @@ class _HouseManagementPageState extends State<HouseManagementPage> {
 void main() {
   runApp(MaterialApp(
     home: HouseManagementPage(),
+    theme: ThemeData(
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+    ),
   ));
 }
